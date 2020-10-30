0610a990c8daf71729b3ee9366f94ee66bf80fad
dabane-ghassan <dabane.ghassan@gmail.com>
2020-10-30 16:16:37 +0100

    factoring installation function

--- blast_setup.py
+++ blast_setup.py
@@ -9,11 +9,22 @@ Created on Fri Oct 30 09:17:12 2020
 import os
 import getpass
 
-msg = "Welcome ! Please provide your machine's sudo password to install blast locally \n \n"
-sudopass = getpass.getpass(msg)
+def install_blast() : 
+    """Installs ncbi-tax+ with a bash command line.
 
-install = 'apt-get install ncbi-blast+'
+    Returns
+    -------
+    None.
+    """
 
-os.system('echo %s|sudo -S %s' % (sudopass, install))
+    msg = "Welcome ! Please provide your machine's sudo password to install blast locally \n \n"
+    sudopass = getpass.getpass(msg)
+    
+    install = 'apt-get install ncbi-blast+'
+    
+    os.system('echo %s|sudo -S %s' % (sudopass, install))
 
+
+if __name__ == "__main__" : 
+    install_blast()
 
