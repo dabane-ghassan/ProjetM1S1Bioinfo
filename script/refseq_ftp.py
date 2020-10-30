bab0fa34252a7bac5e4ed601270c5b58e9c44259
dabane-ghassan <dabane.ghassan@gmail.com>
2020-10-31 00:08:28 +0100

    factoring download_genome and adding some other functions

--- script/refseq_ftp.py
+++ script/refseq_ftp.py
@@ -1,33 +1,52 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 19 11:23:56 2020
-@author: Ghassan Aka KugelBlitZZZ
+@author: ghassan 
 """
 
-
+import os
+import sys
 import pandas as pd
-import re
 from ftplib import FTP
 
-conn = FTP("ftp.ensemblgenomes.org") #ftp://ftp.ncbi.nlm.nih.gov/genomes/
-conn.login()
-conn.cwd('pub/release-48/bacteria/fasta/bacteria_0_collection/bordetella_pertussis_tohama_i/dna/')
-conn.dir()
+genomes = pd.read_table('../data/list_genomes_refseq.txt', header=1)
+genomes = genomes.fillna('')
+genomes['readable'] = genomes['organism_name'] + ' ' + genomes['infraspecific_name']
 
-try :  
-    file = 'Bordetella_pertussis_tohama_i.ASM19571v1.dna.chromosome.Chromosome.fa.gz'
-    with open(file, 'wb') as fp :
-        conn.retrbinary('RETR %s' %file, fp.write)
-except : 
-    print('error')
+inp = input('Please provide a pattern to find if a genome exists in refseq \n')
+pattern = '%s' % (inp)
+path = genomes.loc[(genomes['readable'].str.contains(pattern)==True),'ftp_path']
+path.values[0]
+
+def download_genome(ftp_path) : 
+    """This function downloads a FTP-genome file given a specific ftp address.
+    
+    Parameters
+    ----------
+    ftp_path : TYPE str
+        DESCRIPTION.
+
+    Returns
+    -------
+    None.
+    
+    """
+    with FTP('ftp.ncbi.nlm.nih.gov') as conn : 
+        conn.login()
+        conn.cwd(ftp_path[27:])
+        conn.dir()
+        
+        try :  
+            genome = '%s_genomic.fna.gz' %(ftp_path.rsplit('/')[-1])
+            with open(genome, 'wb') as fp :
+                conn.retrbinary('RETR %s' %genome, fp.write)
+        except : 
+            print('error, cannot download genome file')
+
+
+if __name__ == "__main__" : 
+    
+    project_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+    os.chdir(project_dir)
     
 
-# conn.nlst() pour avoir la liste des fichiers disponibles
-
-
-genomes = pd.read_table('../data/list_genomes_refseq.txt', header=1)
-genomes
-
-
-path = genomes.loc[(genomes['organism_name'] == 'Homo sapiens'),'ftp_path'].values[0]
-path
