2521fc8630cf1bc8eaef267c3dd5726b6aac9f44
Liuccia <Liuccia@users.noreply.github.com>
2020-10-31 16:40:52 +0100

    Update Programme_1.py

--- scripts/Programme_1.py
+++ scripts/Programme_1.py
@@ -11,14 +11,10 @@
 
 import os
 
-# Décompression des génomes à blaster
-decompression_genome_1 = gunzip ../data/genomes/GCF_000007865.1_ASM786v1_genomic.fna.gz
-decompression_genome_2 = gunzip ../data/genomes/GCF_000009445.1_ASM944v1_genomic.fna.gz
+script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
+project_dir = os.path.dirname(script_dir)
+os.chdir(project_dir) # cd to project directory
 
-# Blast à effectuer
-blastp_g1_g2 = blastp -query ../data/genomes/GCF_000007865.1_ASM786v1_genomic.fna -subject ../data/genomes/GCF_000009445.1_ASM944v1_genomic.fna -outfmt 6 > ../data/result_blast/blastp_g1_g2.blast
-
-# Lancement commandes de decompression et de blast
-os.system(decompression_genome_1)
-os.system(decompression_genome_2)
+# Blast
+blastp_g1_g2 = blastp -query /data/genomes/GCF_000007865.1_ASM786v1_genomic.fna -subject data/genomes/GCF_000009445.1_ASM944v1_genomic.fna -outfmt 6 > ../data/result_blast/blastp_g1_g2.blast
 os.system(blast)
