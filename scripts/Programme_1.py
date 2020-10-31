4caf5ff5ef8c1d151b4ace9bfb460ee464de6dff
Liuccia <Liuccia@users.noreply.github.com>
2020-10-31 17:11:14 +0100

    Update Programme_1.py

--- scripts/Programme_1.py
+++ scripts/Programme_1.py
@@ -10,11 +10,12 @@
 """
 
 import os
+import sys
 
 script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 project_dir = os.path.dirname(script_dir)
 os.chdir(project_dir) # cd to project directory
 
 # Blast
-blastp_g1_g2 = blastp -query /data/genomes/GCF_000007865.1_ASM786v1_genomic.fna -subject data/genomes/GCF_000009445.1_ASM944v1_genomic.fna -outfmt 6 > ../data/result_blast/blastp_g1_g2.blast
-os.system(blast)
+blastp_g1_g2 = 'blastp -query /data/genomes/"GCF_000007865.1_ASM786v1_genomic.fna" -subject /data/genomes/"GCF_000009445.1_ASM944v1_genomic.fna" -outfmt 6 > /data/result_blast/blastp_g1_g2.blast'
+os.system(blastp_g1_g2)
