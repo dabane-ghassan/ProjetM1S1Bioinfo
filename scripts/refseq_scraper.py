77e5425d8587b606841c13e4a19f79da702c9847
dabane-ghassan <dabane.ghassan@gmail.com>
2020-11-02 20:06:40 +0100

    adding doc to refseqscraper

--- scripts/refseq_scraper.py
+++ scripts/refseq_scraper.py
@@ -17,6 +17,14 @@ from ftplib import FTP
 class RefSeqScraper : 
     
     def __init__(self) :
+        """This function is used for instantiating RefSeqScraper objects,
+        it will scan refseq summary assembly dataframe, pick only latest and 
+        complete genomes and then will create a new column 'readable' that is
+        the combination of organism and infraspecific name columns.
+        This class has 2 properties : 
+            data : a pandas dataframe that contains all refseq genomes.
+            cart : a list of species names to download their genomes.
+        """
         
         __summary = pd.read_table('data/list_genomes_refseq.txt', header=1)
         __summary = __summary.fillna('')
@@ -29,24 +37,45 @@ class RefSeqScraper :
         self.cart = []
         
     def __str__(self) :
+        """Returns a string representation of the object's cart, empty 
+        otherwise.
+        """
         
         return "Your cart is empty" if len(
             self.cart) == 0 else "Your cart contains %s" % self.cart 
 
 
     def add_to_cart(self, species) : 
+        """ this function adds a species to the object's cart.
+
+        Parameters
+        ----------
+        species : TYPE str
+            DESCRIPTION. the name of the species to be downloaded,
+                        should match, records in the dataframe.
+
+        Returns
+        -------
+        TYPE str
+            DESCRIPTION. a sentence that confirms the addition of a given
+                        species to the cart.
+        """
         
         self.cart.append(species)
         return "added %s to cart" % (species)    
        
     def mine_species(self) :
+        """Asks the user to give a few letters and finds a few records in the
+        dataframe that correspond to the input pattern, if the user would like
+        to add something that he sees, it does it.
+        """
                 
         searching = True
         while searching : 
             pattern = input('Give me a few letters so i can search for you.\n')
             print(self.data.loc[(self.data['readable'].str.contains(pattern)),
                           'readable'].to_string())
-            question = input("Did you find what you're looking for? (yes/no) \n ")
+            question = input("Did you find what you're looking for? (yes/no)\n")
             if  question == "yes" : 
                 searching = False
                 answer = input("which of them would you like to add? \n")
@@ -55,12 +84,25 @@ class RefSeqScraper :
 
         
     def mine_ftps(self) : 
+        """This function searches the dataframe and extracts the ftps that 
+        correspond to species in the cart.
+        
+
+        Returns
+        -------
+        TYPE list
+            DESCRIPTION. a list of ftp paths
+
+        """
 
         return self.data.loc[(self.data['readable'].str.contains('|'.join(self.cart))),
                            'ftp_path'].values
 
 
     def download_genome(self) : 
+        """Downloads all genomes from the ftps paths in the cart and saves 
+        them to data/genomes directory.   
+        """
 
         
         for name, ftp_path in zip(self.cart, self.mine_ftps()) : 
