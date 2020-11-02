94008224f6e0dddbdfd49ba0c6b6ee252159136a
Liuccia <Liuccia@users.noreply.github.com>
2020-11-02 14:02:08 +0100

    Update Programme_1.py

--- scripts/Programme_1.py
+++ scripts/Programme_1.py
@@ -7,18 +7,6 @@
     	a. lance une recherche BLAST d'un génome requête contre un autre génome cible (paramètres)
     	b. lit le fichier résultat afin d'en récupérer la liste des meilleurs hits pour chaque protéine
     	c. lance la recherche BLAST réciproque afin d'en déduire la liste des hits bidirectionnels
-
-peut être utiliser subprocess pour accueillir l'output de blast dans un variable python direct???
-proposition des fonctions
-
-1. fonction blast : 
-    
-test : 
-blast1 = blast("GCF_000007865.1_ASM786v1_genomic.fna","GCF_000009445.1_ASM944v1_genomic.fna", typ="n")
-os.system(blast1)
-    
-2. fonction best hit
-
 """
 
 def blast(query, subject, outfmt=6, typ="p") :
