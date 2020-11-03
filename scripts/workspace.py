bf2e4681675784c97b18267a471a1319e09d38df
dabane-ghassan <dabane.ghassan@gmail.com>
2020-11-03 13:19:45 +0100

    renomme test.py à workspace.py

--- scripts/workspace.py
+++ scripts/workspace.py
@@ -5,12 +5,13 @@ Created on Tue Nov  3 12:46:59 2020
 
 @author: ghassan
 """
+
 import os 
+os.chdir("/home/ghassan/M1/ProjetM1S1Bioinfo/scripts") # mégane change le chemin pour que ça colle avec ton ordi
 from seqkit_stats import stats
 from parse_proteome import parse_fasta
 from programme_un import blast
 
-os.chdir("/home/ghassan/M1/ProjetM1S1Bioinfo/scripts") # mégane change le chemin pour que ça colle avec ton ordi
 
 proteome1 = "../data/genomes/Yersinia_pestis_strain=FDAARGOS_603GCF_003798205.1_ASM379820v1_protein.faa"
 proteome2 = "../data/genomes/Aliivibrio_salmonicida_LFI1238_strain=LFI1238GCF_000196495.1_ASM19649v1_protein.faa" 
@@ -21,4 +22,7 @@ stats(proteome2)
 prot1 = parse_fasta(proteome1)
 prot2 = parse_fasta(proteome2)
 
-os.system(blast(proteome1,proteome2))
+cmdblast1 = blast(proteome1,proteome2, "blast_pestis_salmonicide.blast")
+cmdblast1
+
+os.system(cmdblast1)
