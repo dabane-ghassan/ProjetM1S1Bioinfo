67a650f555eb8fd59f565651e73cc70819bab80e
dabane-ghassan <dabane.ghassan@gmail.com>
2020-11-03 12:44:08 +0100

    ajout documentation script parse_fasta

--- scripts/parse_proteome.py
+++ scripts/parse_proteome.py
@@ -7,6 +7,20 @@ Created on Tue Nov  3 12:35:02 2020
 """
 
 def parse_fasta(proteome) : 
+    """This function parses genome or proteome fasta files.
+
+    Parameters
+    ----------
+    proteome : TYPE str
+        DESCRIPTION. the file path for a given fasta file
+
+    Returns
+    -------
+    seqdic : TYPE dict
+        DESCRIPTION. a dictionary of the proteome with the fasta headers as 
+                    keys and the corresponding fasta sequences as values.
+
+    """
     
     list_seqs = open(proteome, 'r').read().split('>')[1:]  # split the file
     seqdic = {}
