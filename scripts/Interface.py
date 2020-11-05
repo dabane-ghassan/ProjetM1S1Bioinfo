6f0a32a01c807323bce02d867d0dd294e923642e
Liuccia <Liuccia@users.noreply.github.com>
2020-11-05 11:06:38 +0100

    Création interface graphique

---
+++ scripts/Interface.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from tkinter import *
+
+window = Tk()
+
+# En bas de l'interface :
+    # Bouton pour aller sélectionner des protéomes à télécharger
+    # Bouton pour revenir à l'interface principale
+
+
+# Dans la principal interface de départ :
+    # Affichage des protéomes présents dans le disque avec systeme de cochage
+
+
+
+window.mainloop()
