0b2bca23af6e16656cb3ef1d95b392cecfd1e9f3
Liuccia <Liuccia@users.noreply.github.com>
2020-11-05 12:44:05 +0100

    Update Interface.py

--- scripts/Interface.py
+++ scripts/Interface.py
@@ -2,17 +2,11 @@
 # -*- coding: utf-8 -*-
 
 def proteome_in_disk() :
-    list_proteomes = os.listdir(Users/megane/Documents/GitHub/ProjetM1S1Bioinfo/data/genomes)
-    v = IntVar
+    list_proteomes = os.listdir("../data/genomes/")
     i=0
     for p in list_proteomes :
-        case = Checkbutton(variable=v, value=i, text=p)
+        p_presents.insert(i, p)
         i+=1
-
-def proteome_select() :
-    case.select()
-    case.deselect()
-    return v.get()
 
 from tkinter import *
 import os
@@ -23,9 +17,13 @@ window = Tk()
 top = Canvas(window, bg='ivory')
 top.pack(side= TOP, padx=5, pady=5)
 
-# Dans la principal interface de départ :
-    # Affichage des protéomes présents dans le disque avec systeme de cochage
-affichage_l_prot_in_disk = Label(text=proteome_in_disk)
+# Main interface :
+    # Affichage des protéomes présents dans le disque avec systeme de sélection
+p_presents = Listbox(top ,selectmode=MULTIPLE)
+p_presents.insert(1,"Bonjour")
+proteome_in_disk()
+
+prot_select = p_presents.curselection()
 
 # En bas de l'interface :
     # Bouton pour aller sélectionner des protéomes à télécharger
@@ -33,7 +31,7 @@ B_Download = Button(text="Download proteome")
 B_Download.pack(side= LEFT, padx=15)
 
     # Bouton pour revenir à l'interface principale
-B_interface_principale = Button(text="Back to main interface")
+B_interface_principale = Button(text="Back to main interface", command=proteome_in_disk)
 B_interface_principale.pack(side= RIGHT, padx=30)
 
 window.mainloop()
