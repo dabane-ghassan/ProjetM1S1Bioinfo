53edb7910917d93758d24ceb4223710205a4b533
Liuccia <Liuccia@users.noreply.github.com>
2020-11-05 13:28:42 +0100

    Update Interface.py

--- scripts/Interface.py
+++ scripts/Interface.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 def proteome_in_disk() :
+    p_presents.delete(0,END)
     list_proteomes = os.listdir("../data/genomes/")
     i=0
     for p in list_proteomes :
@@ -13,15 +14,11 @@ import os
 
 window = Tk()
 
-# Création d'un canvas
-top = Canvas(window, bg='ivory')
-top.pack(side= TOP, padx=5, pady=5)
-
 # Main interface :
     # Affichage des protéomes présents dans le disque avec systeme de sélection
-p_presents = Listbox(top ,selectmode=MULTIPLE)
-p_presents.insert(1,"Bonjour")
+p_presents = Listbox(window ,selectmode=MULTIPLE, width=80, bg='white', highlightcolor='grey')
 proteome_in_disk()
+p_presents.pack()
 
 prot_select = p_presents.curselection()
 
