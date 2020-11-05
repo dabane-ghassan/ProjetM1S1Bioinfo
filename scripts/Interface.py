feade930a49da720eae1733a81c3c65b18656533
Liuccia <Liuccia@users.noreply.github.com>
2020-11-05 11:31:27 +0100

    Update Interface.py

--- scripts/Interface.py
+++ scripts/Interface.py
@@ -1,18 +1,24 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+def proteome_in_disk() :
+    list_proteomes = os.listdir(/Users/megane/Documents/GitHub/ProjetM1S1Bioinfo/data/genomes)
+    for p in list_proteomes :
+        print p
+
+
 from tkinter import *
+import os
 
 window = Tk()
-
-# En bas de l'interface :
-    # Bouton pour aller sélectionner des protéomes à télécharger
-    # Bouton pour revenir à l'interface principale
-
 
 # Dans la principal interface de départ :
     # Affichage des protéomes présents dans le disque avec systeme de cochage
 
+Label(text=proteome_in_disk)
 
+# En bas de l'interface :
+    # Bouton pour aller sélectionner des protéomes à télécharger
+    # Bouton pour revenir à l'interface principale
 
 window.mainloop()
