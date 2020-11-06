fc1050f1fa5e2790c7376c72ed8c2ec290d16301
dabane-ghassan <dabane.ghassan@gmail.com>
2020-11-06 13:22:13 +0100

    script de travail principale project_skeleton.py

---
+++ scripts/project_skeleton.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from blast_hitter import BlastHitter
+from itertools import combinations
+from clusterer import Clusterizer
+
+
+
+proteomes = ["../data/genomes/Rickettsia_rickettsii_str._Arizona_strain=Arizona_protein.faa",            
+"../data/genomes/Streptococcus_pneumoniae_R6_strain=R6_protein.faa",
+"../data/genomes/Streptococcus_pyogenes_strain=NCTC8232_protein.faa",
+"../data/genomes/Streptococcus_thermophilus_LMD-9_strain=LMD-9_protein.faa",
+"../data/genomes/Piscirickettsia_salmonis_strain=Psal-158_protein.faa"]
+
+prots = list(combinations(proteomes, 2))
+
+prots
+
+BlastHitter.parse_fasta(proteomes[1])
+
+# stats de protéomes
+BlastHitter.seqkit_stats(proteomes[0])
+BlastHitter.seqkit_stats(proteomes[1])
+BlastHitter.seqkit_stats(proteomes[2])
+BlastHitter.seqkit_stats(proteomes[3])
+BlastHitter.seqkit_stats(proteomes[4])
+
+
+blast_hitters = [BlastHitter(couple[0], couple[1]) for couple in prots]
+
+print(blast_hitters[7])
+
+# pas relancer ce code
+"""
+for bh in blast_hitters : 
+    bh.blast_them()
+    bh.rbh_them()
+"""
+#####################   
+    
+rbh = ["../data/results_blast/RBH_Rickettsia_rickettsii_str._Arizona_strain=Arizona_Piscirickettsia_salmonis_strain=Psal-158.blastp",
+       "../data/results_blast/RBH_Rickettsia_rickettsii_str._Arizona_strain=Arizona_Streptococcus_pneumoniae_R6_strain=R6.blastp",
+       "../data/results_blast/RBH_Rickettsia_rickettsii_str._Arizona_strain=Arizona_Streptococcus_pyogenes_strain=NCTC8232.blastp",
+       "../data/results_blast/RBH_Rickettsia_rickettsii_str._Arizona_strain=Arizona_Streptococcus_thermophilus_LMD-9_strain=LMD-9.blastp",
+       "../data/results_blast/RBH_Streptococcus_pneumoniae_R6_strain=R6_Piscirickettsia_salmonis_strain=Psal-158.blastp",
+       "../data/results_blast/RBH_Streptococcus_pneumoniae_R6_strain=R6_Streptococcus_pyogenes_strain=NCTC8232.blastp",
+       "../data/results_blast/RBH_Streptococcus_pneumoniae_R6_strain=R6_Streptococcus_thermophilus_LMD-9_strain=LMD-9.blastp",
+       "../data/results_blast/RBH_Streptococcus_pyogenes_strain=NCTC8232_Piscirickettsia_salmonis_strain=Psal-158.blastp",
+       "../data/results_blast/RBH_Streptococcus_pyogenes_strain=NCTC8232_Streptococcus_thermophilus_LMD-9_strain=LMD-9.blastp",
+       "../data/results_blast/RBH_Streptococcus_thermophilus_LMD-9_strain=LMD-9_Piscirickettsia_salmonis_strain=Psal-158.blastp",         
+       ]
+
+
+
+
+
+
+hi = Clusterizer.clustering(rbh)
+hi
+
+
+
